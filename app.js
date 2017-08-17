b96baa20cabc5e5e46f785be1a11846218d0cd29
Gil Pratte <gil@theironyard.com>
2017-08-17 15:40:41 -0500

    express is working

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('hello')
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('I am alive')
+})
+
+app.listen(3000, function () {
+  console.log('Successfully started express application!');
+})
