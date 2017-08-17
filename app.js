064e7727521e0c2d1b52229303bfc63cf52c0cee
Gil Pratte <gil@theironyard.com>
2017-08-17 16:56:11 -0500

    pseudo code

--- app.js
+++ app.js
@@ -17,8 +17,19 @@ app.use(session({
   saveUninitialized: true
 }))
 
+// create an array of usernames/passwords
+const username_password = []
+
 app.use(function (req, res, next) {
   console.log('in interceptor');
+  // if req.url == '/login'
+	// 	next()
+	// else if !req.session.username
+	// 	res.render('login')
+	// else
+	// 	next()
+
+  // this call to next goes away when you replace the pseudo code with real code
   next()
 })
 
@@ -29,6 +40,16 @@ app.get('/', function (req, res) {
 app.post('/login', function (req, res) {
   console.log("username is " + req.body.username);
   console.log("password is " + req.body.password);
+
+  // loop through array to match username and password
+  // if you find a match then set req.session.login = true
+  // req.session.username = req.body.username
+  // res.render('home')
+
+  // BUT if did not find match
+  // res.render('login', pass error message to mustache)
+
+  // This code to render home goes away when you replace the pseudo code with real code
   res.render('home')
 })
 
