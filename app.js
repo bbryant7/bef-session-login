af96fd427d2fdab33a003460e45a61e969635c94
Gil Pratte <gil@theironyard.com>
2017-08-17 15:50:05 -0500

    home page

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const mustacheExpress = require('mustache-express');
+
 const app = express();
 
+app.engine('mustache', mustacheExpress());
+app.set('views', './views')
+app.set('view engine', 'mustache')
+
 app.get('/', function (req, res) {
-  res.send('I am alive')
+  res.render('home')
 })
 
 app.listen(3000, function () {
