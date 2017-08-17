256ec0fececea13cb36083c1301222b20973ef62
Gil Pratte <gil@theironyard.com>
2017-08-17 16:35:00 -0500

    installed express-session

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
 const bodyParser = require('body-parser');
+const session = require('express-session')
 
 const app = express();
 
@@ -10,13 +11,19 @@ app.engine('mustache', mustacheExpress());
 app.set('views', './views')
 app.set('view engine', 'mustache')
 
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}))
+
 app.use(function (req, res, next) {
   console.log('in interceptor');
   next()
 })
 
 app.get('/', function (req, res) {
-  res.render('login')
+  res.render('home')
 })
 
 app.post('/login', function (req, res) {
